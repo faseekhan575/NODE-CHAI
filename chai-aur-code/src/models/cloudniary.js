58fd39d46b94de0b5f26d0a7aa8afab9b271d563
FASEE KHAN <su92-bscsm-f23-275@gmail.com>
2026-01-26 22:14:15 +0500

    adding miidlware

---
+++ chai-aur-code/src/models/cloudniary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+
+    
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME_CLOUDNIARY, 
+        api_key: process.env.API_KEY_CLOUDNIARY, 
+        api_secret: process.env.API_SCERET_CLOUDNIARY, 
+    });
+
+
+      const uplodeImage=async (filepath)=>{
+          try {
+             const uploadResult = await cloudinary.uploader
+       .upload(
+           'filepath', {
+               public_id: 'shoes',
+           }
+       )
+       console.log('uploadResult',  uploadResult )
+       
+          } catch (error) {
+            fs.unlinkSync(filepath);
+          }
+      }
+   
+
+      export default uplodeImage
